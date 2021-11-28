4443b56e930444a2ec38041426e32f7ab31203c1
Kronenadler <66969855+Kronenadler@users.noreply.github.com>
2021-11-28 14:41:29 +0100

    Some untested development on chat & friends

--- Aufgabe4_Angular/src/app/components/friends/friends.component.ts
+++ Aufgabe4_Angular/src/app/components/friends/friends.component.ts
@@ -1,15 +1,83 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { Friend } from 'src/app/models/Friend';
+import { ContextService } from 'src/app/services/context.service';
+import { BackendService } from '../../services/backend.service';
 
 @Component({
     selector: 'app-friends',
     templateUrl: './friends.component.html',
     styleUrls: ['./friends.component.css']
 })
+
 export class FriendsComponent implements OnInit {
 
+    public user: User;
+    public friends: Array<Friend>;
+    public backendService: BackendService;
+
+    public friendToAdd: string;
+
+    /*
+     * Initialize Site
+     */
     public constructor() {
+        this.backendService = new BackendService(new HttpClient(), new ContextService);
+
+        this.user = new User();
+        this.friends = new Array();
+
+        this.friendToAdd = "";
+
+        // Test
+        /*this.user = new User();
+        this.user.username = "Mickie";
+        this.user.friends = ["Tom", "Peter"];*/
+        //this.user.friends = new Array("Tom", "Peter");
+
     }
 
-    public ngOnInit(): void {
+    public ngOnInit(): void {        
+        this.loadData();
+    }
+
+    public loadData(): void {
+        // Load User
+        this.backendService.loadCurrentUser().then((usr: User|null) => {
+            if(usr != null) {
+                console.log("Loaded User"); //Todo
+                this.user = usr;
+            } else {
+                console.log("Failed loading current User!");
+            }
+        });
+
+        // Load friends
+        this.backendService.loadFriends().then((friends: Array<Friend>) => {
+            if(friends.length > 0) {
+                console.log("Loaded Friends"); //Todo
+                this.friends = friends;
+            }
+        });
+    }
+
+    /*
+     * Interact with friends
+     */
+    public chatFriend(friend: string): void {
+
+    }
+
+    public acceptFriend(friend: string): void {
+        this.backendService.acceptFriendRequest(friend);
+    }
+
+    public declineFriend(friend: string): void {
+        this.backendService.dismissFriendRequest(friend);
+    }
+
+    public addFriend(): void {
+        console.log(this.friendToAdd);
     }
 }

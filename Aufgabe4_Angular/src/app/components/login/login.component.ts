3dc3e18d4faacdde9b9363537e0e59810e4a2e10
KeineAhnungGGWP <goldenideaofficial@gmail.com>
2021-12-01 17:06:28 +0100

    Login funktioniert jetz auch

--- Aufgabe4_Angular/src/app/components/login/login.component.ts
+++ Aufgabe4_Angular/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
     selector: 'app-login',
@@ -7,19 +8,27 @@ import { Router } from '@angular/router';
     styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-    public constructor(public router: Router) { 
+    public username: string = '';
+    public password: string = '';
+    public constructor(public router: Router, private backendservice: BackendService) { 
     }
 
     public ngOnInit(): void {
     }
 
     public login(): void {
-        this.router.navigate(['/friends']);
+        this.backendservice.login(this.username, this.password)
+        .then((result) => {
+            if (result) {
+                console.log("Login passt");
+                this.router.navigate(['/friends']);
+            }
+        })
+        
     }
 
     public register(): void {
         this.router.navigate(['/register']);
     }
-
+ 
 }

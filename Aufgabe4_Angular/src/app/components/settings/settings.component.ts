316ea2751987454aea08bc7f66f4c288b84a9463
DMayer99 <mayer.david99@googlemail.com>
2021-12-02 13:04:53 +0100

    Settings und Profile weiter machen, ServerId anpassen

--- Aufgabe4_Angular/src/app/components/settings/settings.component.ts
+++ Aufgabe4_Angular/src/app/components/settings/settings.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Profile } from 'src/app/models/Profile';
+import { Router } from '@angular/router';
+import { BackendService } from 'src/app/services/backend.service';
 
 
 @Component({
@@ -9,20 +11,34 @@ import { Profile } from 'src/app/models/Profile';
 })
 export class SettingsComponent implements OnInit {
 
-    public profile: Profile; 
+    public profile: Profile = new Profile('', '', '', '', '');
 
-    public constructor() {
-        this.profile = new Profile('', '', '', '', '');
+    public constructor(public router: Router, private backendservice: BackendService) {
     }
 
     public ngOnInit(): void {
+        this.backendservice.loadCurrentUser().then((user: any) => {
+            this.profile.firstName = user != null ? user.firstName : '';
+            this.profile.lastName = user != null ? user.lastName : '';
+            this.profile.coffeeOrTea = user != null ? user.coffeeOrTea : '';
+            this.profile.description = user != null ? user.description : '';
+            this.profile.layout = user != null ? user.layout : '';
+        })
     }
 
     public saveSettings(): void{
-
+        this.backendservice.saveCurrentUserProfile(this.profile)
+        .then((result) => {
+            if (result) {
+                this.router.navigate(['/friends']);
+            }
+            else{
+                console.log("Fehler beim Speichern des Profils");
+            }
+        })
     }
 
     public cancelSettings(): void{
-
+        this.router.navigate(['/friends']);
     }
 }

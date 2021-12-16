2ea80c745a1d1dbe71d339f283310cc78e400279
KeineAhnung1337 <goldenideaofficial@gmail.com>
2021-12-16 15:04:27 +0100

    added login.php

---
+++ Aufgabe5/src/login.php
@@ -0,0 +1,29 @@
+<?php 
+require("start.php");
+
+
+if (!isset($_SESSION["loginData"])) {
+  $_SESSION["loginData"] = array();
+}
+  $username = $_POST["username"];
+  $password = $_POST["pwd"];
+  if (empty($username) ||empty($password)) {
+     $error = "Somethign went wront";
+  } 
+  else {
+   // $error = "everythigns fine";
+  }
+
+
+if(login($username,$password)) {
+  $_SESSION["user"] = $this->username;
+  header("Location: friends.php");
+}
+else {
+  $error = "Fehler in der Registrierung.";
+}
+?>
+
+
+
+

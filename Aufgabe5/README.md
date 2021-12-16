c7683e17e1bd53eb17b35274b273b6e01697072c
Kronenadler <66969855+Kronenadler@users.noreply.github.com>
2021-12-16 15:40:30 +0100

    Added Server / Collection ID

---
+++ Aufgabe5/README.md
@@ -0,0 +1,7 @@
+# Link to Chat Server API:
+
+https://online-lectures-cs.thi.de/chat/full/0be8ffd7-60bb-4f00-84fc-4e8a9cb53aa3
+
+# Chat Token
+
+0be8ffd7-60bb-4f00-84fc-4e8a9cb53aa3
--- Aufgabe5/src/start.php
+++ Aufgabe5/src/start.php
@@ -2,7 +2,7 @@
 session_start();
 
 define('CHAT_SERVER_URL', 'https://online-lectures-cs.thi.de/chat');
-define('CHAT_SERVER_ID', '...'); # Ihre Collection ID
+define('CHAT_SERVER_ID', '0be8ffd7-60bb-4f00-84fc-4e8a9cb53aa3'); # Ihre Collection ID
 
 
 spl_autoload_register(function($class) {
